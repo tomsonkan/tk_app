ba5c1003f096bde12cc24bdf9f813482deffc9d3
Tomson Kan <tomson.kan@gmail.com>
2020-04-12 00:56:05 -0500

    update format

--- src/components/Home.js
+++ src/components/Home.js
@@ -40,28 +40,30 @@ const Home = (props) => {
     
   // }
 
-  const theme = createMuiTheme({
-    palette: {
-        primary: { main: '#228e22' },
-        secondary: { main: '#4d4d4d' },
-    },
-    typography: {
-        useNextVariants: true,
-        fontFamily: '"Montserrat", Arial, Helvetica, sans-serif',
-    },
-})
+//   const theme = createMuiTheme({
+//     palette: {
+//         primary: { main: '#228e22' },
+//         secondary: { main: '#4d4d4d' },
+//     },
+//     typography: {
+//         useNextVariants: true,
+//         fontFamily: '"Montserrat", Arial, Helvetica, sans-serif',
+//     },
+// })
 
   function refreshPage() {
     window.location.reload(false);
   }
 
-  function highCount() {
-
+  const styles = {
+    paperContainer: {
+        backgroundImage: `url(${"https://img.wallpapersafari.com/desktop/1600/900/6/39/01gilJ.png"})`
+    }
   }
 
   const TextStyle = {
     fontWeight: 'bold'
-}
+  }
 
 console.log(props.reqs)
 console.log(props.reqs.length)
@@ -71,7 +73,8 @@ console.log(props.reqs.length)
     <>
     {document.cookie == "loggedIn=true" ? ( 
     <>
-   
+    <div className="wrapper">
+      <div className="slide">
     <Container maxWidth="lg" className="car-container" style={{height: "60px", width: "500px", marginTop:"50px", paddingBottom: "200px", justifyContent:"center"}}>
       <br/>
       <div className = "flex-container">
@@ -84,20 +87,19 @@ console.log(props.reqs.length)
       </div>
     </Container>
     <div className="card-container1" style={{ marginBottom: "50px"}}>
-    <Container><h2 className= "ticTitle">High Priority Tickets: <span style={{fontSize: '30px', fontWeight: "Normal", color: "Red"}}>{props.reqs.filter(item => item.lvl === 'High').length}</span></h2></Container> 
+    <Container><h2 className= "ticTitle">High Priority Tickets: <span style={{fontSize: '30px', fontWeight: "Bold"}}>{props.reqs.filter(item => item.lvl === 'High').length}</span></h2></Container> 
       {props.reqs.length === 0 ? props.fetchReqs() :  
           props.reqs.map((req, idx) => (
             <Card key={req.id} className="card">
             {req["lvl"] == "High" ? 
               <>
-              <CardContent className="text-gray1" theme = {theme}>
-                <Grid container alignItems="flex-start" justify="space-evenly" direction="row">
+              <CardContent className="text-gray1">
+                <Grid container alignItems="flex-start" justify="space-evenly" direction="row" >
                   <h2 className= "ticTitle">BUG ID:  </h2>
-                  <Avatar className = "av1" aria-label="recipe"  style={{color: 'mediumblue'}}>
+                  <Avatar className = "av" aria-label="recipe"  style={{color: 'mediumblue'}}>
                   {req["id"]}
                   </Avatar>
                 </Grid>
-                
                 <ul >  
                   <li className = "unlist"><span style={{fontWeight: "Bold"}}>Bug Type:</span> {req["Bug"]}</li>
                   <li className = "unlist"><span style={{fontWeight: "Bold"}}>First Name:</span> {req["first_name"]}</li>
@@ -127,7 +129,7 @@ console.log(props.reqs.length)
     </div> 
     
     <div className="card-container2" style={{marginTop:"20px", marginBottom: "40px"}}>
-      <Container><h2 className= "ticTitle">Medium Priority Tickets: <span style={{fontSize: '30px', fontWeight: "Normal", color: "Blue"}}>{props.reqs.filter(item => item.lvl === 'Medium').length}</span></h2></Container>
+      <Container><h2 className= "ticTitle">Medium Priority Tickets: <span style={{fontSize: '30px', fontWeight: "Bold"}}>{props.reqs.filter(item => item.lvl === 'Medium').length}</span></h2></Container>
       {props.reqs.length === 0 ? props.fetchReqs() :props.reqs.map((req, idx) => (
         <Card key={idx} className="card1">
           {req["lvl"] == "Medium" ? 
@@ -135,7 +137,7 @@ console.log(props.reqs.length)
             <CardContent className="text-gray2" >
               <Grid container alignItems="flex-start" justify="space-evenly" direction="row">
                 <h2 className= "ticTitle">BUG ID:</h2>
-                <Avatar className = "av2" aria-label="recipe"  style={{color: 'mediumblue'}}>
+                <Avatar className = "av" aria-label="recipe"  style={{color: 'mediumblue'}}>
                 {req["id"]}
                 </Avatar>
               </Grid>
@@ -164,8 +166,8 @@ console.log(props.reqs.length)
       ))}
     </div>  
 
-    <div className="card-container3" style={{marginTop:"20px", marginBottom: "40px"}}>
-      <Container><h2 className= "ticTitle">Low Priority Tickets: <span style={{fontSize: '30px', fontWeight: "Normal", color: "Green", paddingBottom: "100px"}}>{props.reqs.filter(item => item.lvl === 'Low').length}</span></h2></Container>
+    <div className="card-container3" style={{marginTop:"20px", marginBottom: "100px"}}>
+      <Container><h2 className= "ticTitle">Low Priority Tickets: <span style={{fontSize: '30px', fontWeight: "Bold", paddingBottom: "100px"}}>{props.reqs.filter(item => item.lvl === 'Low').length}</span></h2></Container>
       {props.reqs.length === 0 ? props.fetchReqs() :props.reqs.map((req, idx) => (
         <Card key={idx} className="card1">
           {req["lvl"] == "Low" ? 
@@ -173,7 +175,7 @@ console.log(props.reqs.length)
             <CardContent className="text-gray3" >
               <Grid container alignItems="flex-start" justify="space-evenly" direction="row">
                 <h2 className= "ticTitle">BUG ID: </h2>
-                <Avatar className = "av2" aria-label="recipe"  style={{color: 'mediumblue'}}>
+                <Avatar className = "av" aria-label="recipe"  style={{color: 'mediumblue'}}>
                 {req["id"]}
                 </Avatar>
               </Grid>
@@ -201,19 +203,16 @@ console.log(props.reqs.length)
         </Card> 
       ))}
     </div>
-    <Container className="foot">
       <div className="foot">
-    
-      <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
-      <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
-      <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
-      <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
-    
-    <h4 className= "ticTitle">SmallBizBugTracker ©2020</h4>
+        <BottomNavigationAction label="Recents" value="recents" icon={<RestoreIcon />} />
+        <BottomNavigationAction label="Favorites" value="favorites" icon={<FavoriteIcon />} />
+        <BottomNavigationAction label="Nearby" value="nearby" icon={<LocationOnIcon />} />
+        <BottomNavigationAction label="Folder" value="folder" icon={<FolderIcon />} />
+        <h4 className= "ticTitle">SmallBizBugTracker ©2020</h4>
+       </div>
     </div>
-    </Container>
+    </div>
   </>
-  
   )
   : null}
   </>
